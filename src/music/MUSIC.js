81a708d77d90a032b3d4edc4eed48808a655c07c
Nuttapol Khumdang <nuttapol.chok@gmail.com>
2024-09-12 18:12:59 +0700

    content: inital music part

---
+++ src/music/MUSIC.js
@@ -0,0 +1,7 @@
+const MUSIC = [
+    {
+        src: "src/music/20240910.mp3",
+        title: "Until We Meet Again",
+        length: "1.29"
+    }
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.html"
-  ],
-  darkMode: "selector",
-  theme: {
-      fontFamily: {
-          display: ["Major Mono Display", "monospace"],
-          sans: ["Poppins", "'IBM Plex Sans Thai Looped'", "sans-serif"],
-          mono: ["DM Mono", "monospace"],
-          monot: ["'IBM Plex Sans Thai Looped'", "monospace"],
-          cursive: ["La Belle Aurore", "cursive"],
-          space: ["Space Mono", "monospace"],
-          ps2p: ["'Press Start 2P'", "monospace"],
-          tagb: ["'TAGameboy'", "monospace"],
-      },
-  },
-  plugins: [],
+    content: [
+        "./*.html"
+    ],
+    theme: {
+        fontFamily: {
+            display: ["Major Mono Display", "monospace"],
+            sans: ["Poppins", "'IBM Plex Sans Thai Looped'", "sans-serif"],
+            mono: ["DM Mono", "monospace"],
+            monot: ["'IBM Plex Sans Thai Looped'", "monospace"],
+            cursive: ["La Belle Aurore", "cursive"],
+            space: ["Space Mono", "monospace"],
+            ps2p: ["'Press Start 2P'", "monospace"],
+            tagb: ["'TAGameboy'", "monospace"],
+        },
+    },
+    plugins: [],
 }
-
